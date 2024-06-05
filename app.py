aa94e786b84d2708d365b801ac405bcd5efb576e
Luji <zlfjrii@gmail.com>
2024-06-05 11:26:43 +0700

    feature: Endpoint for admin login

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 from flask import Flask,redirect,url_for,render_template,request
 
 app=Flask(__name__)
+
 @app.route('/',methods=['GET','POST'])
 def home():
     return render_template('index.html')
+
+@app.route('/admin_login', methods=['GET, POST'])
+def admin_login():
+    return render_template('login.html')
 
 if __name__ == '__main__':
     app.run('0.0.0.0',port=5000,debug=True)
